4f3d259fcb676ac6ad62249be4517fb710cc8300
thusis <102267039+thusis@users.noreply.github.com>
2022-05-07 23:18:14 +0900

    #2.15 Conditionals part Three (13:49)

--- app.js
+++ app.js
@@ -59,3 +59,18 @@ function divide(firstNumber, secondNumber){
     console.log(firstNumber/secondNumber);
 };
 divide(98,20);
+
+//
+
+const age = parseInt(prompt(”How old are you?”));
+
+if(isNaN(age)) {
+console.log(”Please write a number”);
+} else if (age<18) {
+console.log(”You are too young.”);
+} else {
+console.log(”You can drink.”);
+};
+
+&& : and ⇒ one and the other;
+|| : or ⇒ one or the other;
