360f887c655ad1b290608a671bfd61f41c486a07
thusis <102267039+thusis@users.noreply.github.com>
2022-05-12 20:32:44 +0900

    220512 ~#3.5

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-const title=document.getElementById("title");
-console.dir(title);
+const title = document.querySelector("div.text:first-child h1")
+
+function handleMouseEnter(){
+    title.innerText="The mouse is here!";
+    title.style.color="blue";
+}
+function handleMouseLeave(){
+    title.innerText="The mouse is gone!";
+    title.style.color="tomato";
+}
+function handleWindowResize(){
+    title.innerText="You just resized!";
+    title.style.color="darkgreen";
+}
+function handleTitleClick(){
+    title.innerText="That was a right click!";
+    title.style.color="brown";
+}
 
 
-title.innerText="Got you!";
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
+window.addEventListener("resize",handleWindowResize);
+title.addEventListener("click",handleTitleClick);
